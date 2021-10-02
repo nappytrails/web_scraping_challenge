921d6aa839e20f04ea6fdc6f1b3500e3f304915a
Erin Kinney <erinckinney@gmail.com>
2021-10-01 22:12:49 -0700

    Add app.py, scrape_mars.py

---
+++ Missions_to_Mars/app.py
@@ -0,0 +1,8 @@
+# Import dependencies
+import pandas as pd
+import requests
+from splinter import Browser
+from bs4 import BeautifulSoup
+from webdriver_manager.chrome import ChromeDriverManager
+from scrape_mars import scrape
+
