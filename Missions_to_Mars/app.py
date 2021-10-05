a1244efba0d673ea24cbc105ca09e3ce75a9bd7a
Erin Kinney <erinckinney@gmail.com>
2021-10-05 00:06:25 -0700

    Update app.py

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -1,8 +1,53 @@
 # Import dependencies
 import pandas as pd
+from pymongo import database
 import requests
 from splinter import Browser
 from bs4 import BeautifulSoup
 from webdriver_manager.chrome import ChromeDriverManager
-from scrape_mars import scrape
+from flask import Flask, render_template, redirect
+# from flask_pymongo import PyMongo
+import pymongo
+import scrape_mars
+
+# Create an instance of Flask
+app = Flask(__name__)
+
+## Connect to MongoDB using pymongo to create database and collection
+# Create connection variable
+conn = 'mongodb://localhost:27017'
+
+# Pass connection to the pymongo instance.
+client = pymongo.MongoClient(conn)
+
+# Drop existing "mars_db" database if any
+try:
+    client.drop_database("mars_db")
+except:
+    print("No database to drop")
+
+# Create mars_db database
+db = client.mars_db
+
+# Creating the collection "mars_col"
+mars_col = db.mars_col
+
+
+@app.route("/")
+def index():
+    mars_display = mars_col.find()
+    return render_template("index.html", mars_display=mars_display)
+
+
+@app.route("/scrape")
+def scraper():
+
+    # Web scrape various sites about Mars and add results to "mars_col"
+    mars_info = scrape_mars.scrape()
+    mars_col.update({}, mars_info, upsert=True)
+    
+    # Close connection to MongoDB
+    client.close()
+    
+    return redirect("/", code=302)
 
--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -1,161 +1,141 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": 24,
-   "id": "73ae98d4-015a-4a15-826e-51e3443d4062",
-   "metadata": {
-    "tags": []
-   },
-   "outputs": [],
-   "source": [
-    "def scrape():\n",
-    "    '''Scrapes several websites about Mars'''\n",
-    "    \n",
-    "    def init_browser():\n",
-    "        '''Launches a Chrome browser using splinter and ChromeDriverManager'''\n",
-    "        executable_path = {'executable_path': ChromeDriverManager().install()}\n",
-    "        return Browser('chrome', **executable_path, headless=True)\n",
-    "\n",
-    "    def browse(url):\n",
-    "        '''Function to scrape a single webpage'''\n",
-    "        browser = init_browser()\n",
-    "        browser.visit(url)\n",
-    "\n",
-    "        # Scrape html code\n",
-    "        html = browser.html    \n",
-    "\n",
-    "        # Create BeautifulSoup object; parse with 'html.parser'\n",
-    "        soup = BeautifulSoup(html, \"html.parser\")\n",
-    "\n",
-    "        # Quit browser\n",
-    "        browser.quit()\n",
-    "\n",
-    "        return soup\n",
-    "\n",
-    "    # Create an empty dictionary to hold web scraping resutls\n",
-    "    mars_scrape_dict = {}\n",
-    "\n",
-    "    # URL for NASA Mars News Site \n",
-    "    nasa_url = \"https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest\"\n",
-    "\n",
-    "    # Scrape html of nasa_url\n",
-    "    nasa_soup = browse(nasa_url)\n",
-    "\n",
-    "    # Find title and teaser text of first article and store them to variables\n",
-    "    first_article_title = nasa_soup.find(\"div\", class_=\"bottom_gradient\").text\n",
-    "    first_article_teaser = nasa_soup.find(\"div\", class_=\"article_teaser_body\").text\n",
-    "\n",
-    "    # Append the article variables to mars_scrape_dict\n",
-    "    mars_scrape_dict[\"article_title\"] = first_article_title\n",
-    "    mars_scrape_dict[\"article_teaser\"] = first_article_teaser\n",
-    "\n",
-    "    # URL to JPL Mars image website\n",
-    "    jpl_url = \"https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/index.html\"\n",
-    "\n",
-    "    # Scrape jpl_url\n",
-    "    jpl_soup = browse(jpl_url)\n",
-    "\n",
-    "    # Scrape the link to the featured image\n",
-    "    jpl_image_link = jpl_soup.find(\"a\", class_=\"showimg fancybox-thumbs\")[\"href\"]\n",
-    "\n",
-    "    # Assign the full image url to a variable\n",
-    "    featured_image_url = f\"https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/{jpl_image_link}\"\n",
-    "\n",
-    "    mars_scrape_dict[\"featured_image\"] = featured_image_url\n",
-    "\n",
-    "    # Scrape url using pandas and assign scraped tables to a variable\n",
-    "    space_facts_url = \"https://space-facts.com/mars/\"\n",
-    "    tables = pd.read_html(space_facts_url)\n",
-    "\n",
-    "    # Create a dataframe from the first table\n",
-    "    stats_table_df = pd.DataFrame(tables[0])\n",
-    "\n",
-    "    # Rename the columns\n",
-    "    stats_table_df = stats_table_df.rename(columns={0:\"\", 1:\"values\"}, inplace=False)\n",
-    "\n",
-    "    # Set the index to the first column\n",
-    "    stats_table_new_index_df = stats_table_df.set_index(keys=\"\")\n",
-    "\n",
-    "    # Generate html code for the table and remove \"\\n\" from the code\n",
-    "    stats_table_html = stats_table_new_index_df.to_html()\n",
-    "\n",
-    "    stats_table_html = stats_table_html.replace(\"\\n\", \"\")\n",
-    "\n",
-    "    # Append the table to mars_scrape_dict\n",
-    "    mars_scrape_dict[\"table_html\"] = stats_table_html\n",
-    "\n",
-    "    # USGS Mars website url\n",
-    "    usgs_url = \"https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars\"\n",
-    "\n",
-    "    # Initiate browswer instance\n",
-    "    usgs_browser = init_browser()\n",
-    "    usgs_browser.visit(usgs_url)\n",
-    "\n",
-    "    # Scrape image Mars hemispheres pages\n",
-    "    hemispheres = [\"Cerberus\", \"Schiaparelli\", \"Syrtis\", \"Valles\"]\n",
-    "    soup_objects = []\n",
-    "\n",
-    "    for hemisphere in hemispheres:\n",
-    "        usgs_browser.links.find_by_partial_text(hemisphere).click()\n",
-    "\n",
-    "        # Scrape html code\n",
-    "        usgs_html = usgs_browser.html    \n",
-    "\n",
-    "        # Create BeautifulSoup object; parse with 'html.parser'\n",
-    "        usgs_soup = BeautifulSoup(usgs_html, \"html.parser\")\n",
-    "\n",
-    "        # Assign BeautifulSoup object to unique variable; print variable name\n",
-    "        locals()[\"soup_\" + hemisphere] = usgs_soup\n",
-    "\n",
-    "        # Append soup object to list\n",
-    "        soup_objects.append(locals()[\"soup_\" + hemisphere])\n",
-    "\n",
-    "        # Go back to previous page\n",
-    "        usgs_browser.back()\n",
-    "\n",
-    "    usgs_browser.quit()\n",
-    "\n",
-    "    # Find title and url for hemispheres images\n",
-    "    hemispheres_list = []\n",
-    "\n",
-    "    for soup_object in soup_objects:\n",
-    "\n",
-    "        hemispheres_dict = {}\n",
-    "\n",
-    "        title = soup_object.find(\"h2\", class_=\"title\").text.replace(\" Enhanced\", \"\")\n",
-    "        img_url = soup_object.find_all(\"a\", target=\"_blank\")[3][\"href\"]\n",
-    "\n",
-    "        hemispheres_dict[\"title\"] = title\n",
-    "        hemispheres_dict[\"img_url\"] = img_url\n",
-    "\n",
-    "        hemispheres_list.append(hemispheres_dict)\n",
-    "\n",
-    "    mars_scrape_dict[\"hemispheres\"] = hemispheres_list\n",
-    "\n",
-    "    return mars_scrape_dict"
-   ]
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.8.8"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 5
-}
+# Import dependencies
+import pandas as pd
+import requests
+from splinter import Browser
+from bs4 import BeautifulSoup
+from webdriver_manager.chrome import ChromeDriverManager
+
+def scrape():
+    def init_browser():
+        '''Launches a Chrome browser using splinter and ChromeDriverManager'''
+        executable_path = {'executable_path': ChromeDriverManager().install()}
+        return Browser('chrome', **executable_path, headless=True)
+
+    def browse(url):
+        '''Function to scrape a single webpage'''
+        browser = init_browser()
+        browser.visit(url)
+
+        # Scrape html code
+        html = browser.html    
+
+        # Create BeautifulSoup object; parse with 'html.parser'
+        soup = BeautifulSoup(html, "html.parser")
+    
+        # Quit browser
+        browser.quit()
+    
+        return soup
+
+    # Create an empty dictionary to hold web scraping resutls
+    mars_scrape_dict = {}
+
+
+    ##### NASA Mars News #####
+
+    # URL for NASA Mars News Site 
+    nasa_url = "https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest"
+
+    # Scrape html of nasa_url
+    nasa_soup = browse(nasa_url)
+
+    # Find title and teaser text of first article and store them to variables
+    first_article_title = nasa_soup.find("div", class_="bottom_gradient").text
+    first_article_teaser = nasa_soup.find("div", class_="article_teaser_body").text
+
+    # Append the article variables to mars_scrape_dict
+    mars_scrape_dict["article_title"] = first_article_title
+    mars_scrape_dict["article_teaser"] = first_article_teaser
+
+
+    ##### JPL Mars Space Images - Featured Image #####
+
+    # URL to JPL Mars image website
+    jpl_url = "https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/index.html"
+
+    # Scrape jpl_url
+    jpl_soup = browse(jpl_url)
+
+    # Scrape the link to the featured image
+    jpl_image_link = jpl_soup.find("a", class_="showimg fancybox-thumbs")["href"]
+
+    # Assign the full image url to a variable
+    featured_image_url = f"https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/{jpl_image_link}"
+
+    mars_scrape_dict["featured_image"] = featured_image_url
+
+
+    ##### Mars Facts #####
+
+    # Scrape url using pandas and assign scraped tables to a variable
+    space_facts_url = "https://space-facts.com/mars/"
+    tables = pd.read_html(space_facts_url)
+
+    # Create a dataframe from the first table
+    stats_table_df = pd.DataFrame(tables[0])
+
+    # Rename the columns
+    stats_table_df = stats_table_df.rename(columns={0:"", 1:"values"}, inplace=False)
+
+    # Set the index to the first column
+    stats_table_new_index_df = stats_table_df.set_index(keys="")
+
+    # Generate html code for the table and remove "\n" from the code
+    stats_table_html = stats_table_new_index_df.to_html()
+    stats_table_html = stats_table_html.replace("\n", "")
+
+    # Append the table to mars_scrape_dict
+    mars_scrape_dict["table_html"] = stats_table_html
+
+
+    ##### Mars Hemispheres #####
+
+    # USGS Mars website url
+    usgs_url = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
+
+    # Initiate browswer instance
+    usgs_browser = init_browser()
+    usgs_browser.visit(usgs_url)
+
+    # Scrape image Mars hemispheres pages
+    hemispheres = ["Cerberus", "Schiaparelli", "Syrtis", "Valles"]
+    soup_objects = []
+
+    for hemisphere in hemispheres:
+        usgs_browser.links.find_by_partial_text(hemisphere).click()
+
+        # Scrape html code
+        usgs_html = usgs_browser.html    
+
+        # Create BeautifulSoup object; parse with 'html.parser'
+        usgs_soup = BeautifulSoup(usgs_html, "html.parser")
+
+        # Assign BeautifulSoup object to unique variable; print variable name
+        locals()["soup_" + hemisphere] = usgs_soup
+
+        # Append soup object to list
+        soup_objects.append(locals()["soup_" + hemisphere])
+                            
+        # Go back to previous page
+        usgs_browser.back()
+        
+    usgs_browser.quit()
+
+    # Find title and url for hemispheres images
+    hemispheres_list = []
+
+    for soup_object in soup_objects:
+
+        hemispheres_dict = {}
+
+        title = soup_object.find("h2", class_="title").text.replace(" Enhanced", "")
+        img_url = soup_object.find_all("a", target="_blank")[3]["href"]
+        
+        hemispheres_dict["title"] = title
+        hemispheres_dict["img_url"] = img_url
+        
+        hemispheres_list.append(hemispheres_dict)
+
+    mars_scrape_dict["hemispheres"] = hemispheres_list
+
+    return mars_scrape_dict
