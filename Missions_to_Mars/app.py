e40d1d3995911e43e75c24ab9036cb7debcb9b98
Erin Kinney <erinckinney@gmail.com>
2021-10-14 17:27:25 -0700

    Update index

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -1,53 +1,59 @@
 # Import dependencies
-import pandas as pd
-from pymongo import database
-import requests
-from splinter import Browser
-from bs4 import BeautifulSoup
-from webdriver_manager.chrome import ChromeDriverManager
+# import pandas as pd
+# import pymongo
+# from pymongo import database
+# import requests
+# from splinter import Browser
+# from bs4 import BeautifulSoup
+# from webdriver_manager.chrome import ChromeDriverManager
 from flask import Flask, render_template, redirect
-# from flask_pymongo import PyMongo
-import pymongo
+from flask_pymongo import PyMongo
 import scrape_mars
 
 # Create an instance of Flask
 app = Flask(__name__)
 
-## Connect to MongoDB using pymongo to create database and collection
-# Create connection variable
-conn = 'mongodb://localhost:27017'
+# Use PyMongo to establish Mongo connection
+mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
+mars_col = mongo.db.mars_col
 
-# Pass connection to the pymongo instance.
-client = pymongo.MongoClient(conn)
+# ## Connect to MongoDB using pymongo to create database and collection
+# # Create connection variable
+# conn = 'mongodb://localhost:27017'
 
-# Drop existing "mars_db" database if any
-try:
-    client.drop_database("mars_db")
-except:
-    print("No database to drop")
+# # Pass connection to the pymongo instance.
+# client = pymongo.MongoClient(conn)
 
-# Create mars_db database
-db = client.mars_db
+# # Create mars_db database
+# db = client.mars_db
 
-# Creating the collection "mars_col"
-mars_col = db.mars_col
+# # Create the collection "mars_col"
+# mars_col = db.mars_col
 
 
 @app.route("/")
 def index():
-    mars_display = mars_col.find()
+    mars_display = mars_col.find_one()
     return render_template("index.html", mars_display=mars_display)
 
 
 @app.route("/scrape")
 def scraper():
 
+    # Drop existing "mars_db" database if any
+    # try:
+    #     client.drop_database("mars_db")
+    # except:
+    #     print("No database to drop")
+
     # Web scrape various sites about Mars and add results to "mars_col"
     mars_info = scrape_mars.scrape()
     mars_col.update({}, mars_info, upsert=True)
     
     # Close connection to MongoDB
-    client.close()
+    # client.close()
     
     return redirect("/", code=302)
 
+if __name__ == "__main__":
+    app.run(debug=True)
