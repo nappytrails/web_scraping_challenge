c598038c207666931c27ab4aca9423d6634e15bb
Erin Kinney <erinckinney@gmail.com>
2021-10-26 09:52:12 -0700

    Update README

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -1,11 +1,4 @@
 # Import dependencies
-# import pandas as pd
-# import pymongo
-# from pymongo import database
-# import requests
-# from splinter import Browser
-# from bs4 import BeautifulSoup
-# from webdriver_manager.chrome import ChromeDriverManager
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
 import scrape_mars
@@ -17,20 +10,6 @@ app = Flask(__name__)
 mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
 mars_col = mongo.db.mars_col
 
-# ## Connect to MongoDB using pymongo to create database and collection
-# # Create connection variable
-# conn = 'mongodb://localhost:27017'
-
-# # Pass connection to the pymongo instance.
-# client = pymongo.MongoClient(conn)
-
-# # Create mars_db database
-# db = client.mars_db
-
-# # Create the collection "mars_col"
-# mars_col = db.mars_col
-
-
 @app.route("/")
 def index():
     mars_display = mars_col.find_one()
@@ -40,18 +19,10 @@ def index():
 @app.route("/scrape")
 def scraper():
 
-    # Drop existing "mars_db" database if any
-    # try:
-    #     client.drop_database("mars_db")
-    # except:
-    #     print("No database to drop")
-
     # Web scrape various sites about Mars and add results to "mars_col"
     mars_info = scrape_mars.scrape()
     mars_col.update({}, mars_info, upsert=True)
     
-    # Close connection to MongoDB
-    # client.close()
     
     return redirect("/", code=302)
 
--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# web_scraping_challenge
+# All About Mars
+## Summary
 Flask app that web scrapes information on the planet Mars and returns the data on a webpage
+
+## Tech Stack
+__Languages__  
++ Python
++ HTML
++ Jinja
+
+__Python dependencies__
++ pandas
++ requests
++ splinter
++ BeautifulSoup
++ ChromeDriverManager
++ Flask
++ PyMongo (flask_pymongo)
+
+## Data Sources
++ [NASA Mars News Site](https://mars.nasa.gov/news/)
++ [JPL Mars Space Images](https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/index.html)
++ [Space Facts](https://space-facts.com/mars/)
++ [USGS Astrogeology](https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars)
+
+## Final Product
+![index_html.png](Images/index_html.png)
